37ff9c7e92bf7752d3a55c20ed8bfc49dc5f187f
amerkhanfar <amer@oplus.ae>
2024-09-27 13:09:40 +0400

    edited project page

--- js/media-data-handler-ar.js
+++ js/media-data-handler-ar.js
@@ -1,57 +1,71 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const token = '4|eJ1so9HdGTHPOzYkso2TBb04B1YxJNl294zHyIzFb446e2e9';
-    const apiUrl = 'https://app.oplus.dev/api/v1/gef/news/posts';
-    
-    // Fetch the news from the API
-    fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      const newsContainer = document.getElementById('news-container');
-      
+document.addEventListener("DOMContentLoaded", function () {
+  const token = "4|eJ1so9HdGTHPOzYkso2TBb04B1YxJNl294zHyIzFb446e2e9";
+  const apiUrl = "https://app.oplus.dev/api/v1/gef/news/posts";
+
+  // Fetch the news from the API
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const newsContainer = document.getElementById("news-container");
+
       // Iterate over the news articles and create the HTML structure in Arabic
-      data.forEach(article => {
+      data.forEach((article) => {
         const newsHTML = `
           <div class="col-md-4">
-            <div class="news">
+            <div class="news" style='text-align:right'>
               <div class="news-img" style='min-height:250px'>
-                <a href="single-news-ar.html?id=${article.id}" data-triangle=".news-img-overlay">
-                  <span class="news-img-overlay"></span>
-                  <img
-                    src="${article.featured_image}"
-                    alt="${article.title.ar}"
-                    width="370"
-                    height="284"
-                  />
+                <a href="single-news-ar.html?id=${
+                  article.id
+                }" data-triangle=".news-img-overlay">
+
+
+                <div style='width:370px;height:284px; background-size: cover; background-position: center; background-image:url("${
+                  article.featured_image
+                }")'></div>
+               
+
                 </a>
               </div>
               <p class="news-meta">
-                بواسطة
-                <span class="news-author" href="single-news-ar.html?id=${article.id}">${article.author.ar}</span>
+                
+                <span class="news-author" href="single-news-ar.html?id=${
+                  article.id
+                }">${article.author.ar}</span>
                 <span>-</span>
-                <a class="news-time" href="single-news-ar.html?id=${article.id}">
-                  <time datetime="${new Date(article.published_at).toISOString()}">
-                    ${new Date(article.published_at).toLocaleDateString('ar-EG')}
+                <a class="news-time" href="single-news-ar.html?id=${
+                  article.id
+                }">
+                  <time datetime="${new Date(
+                    article.published_at,
+                  ).toISOString()}">
+                    ${new Date(article.published_at).toLocaleDateString(
+                      "ar-EG",
+                    )}
                   </time>
                 </a>
               </p>
-              <h4 class="news-title">
-                <a href="single-news-ar.html?id=${article.id}">${article.title.ar}</a>
-              </h4>
-              <a class="news-link" href="single-news-ar.html?id=${article.id}">اقرأ المزيد</a>
+                <h5 style='font-weight: bold' class="news-title">
+                <a href="single-news-ar.html?id=${article.id}">${
+          article.title.ar
+        }</a>
+              </h5>
+              <a class="news-link" href="single-news-ar.html?id=${
+                article.id
+              }">اقرأ المزيد</a>
             </div>
           </div>
         `;
-        
+
         // Append each article to the container
         newsContainer.innerHTML += newsHTML;
       });
     })
-    .catch(error => {
-      console.error('Error fetching news:', error);
+    .catch((error) => {
+      console.error("Error fetching news:", error);
     });
-  });
+});
--- js/media-data-handler.js
+++ js/media-data-handler.js
@@ -1,32 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const token = '4|eJ1so9HdGTHPOzYkso2TBb04B1YxJNl294zHyIzFb446e2e9';
-    const apiUrl = 'https://app.oplus.dev/api/v1/gef/news/posts';
-    
-    // Fetch the news from the API
-    fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      const newsContainer = document.getElementById('news-container');
-      
+document.addEventListener("DOMContentLoaded", function () {
+  const token = "4|eJ1so9HdGTHPOzYkso2TBb04B1YxJNl294zHyIzFb446e2e9";
+  const apiUrl = "https://app.oplus.dev/api/v1/gef/news/posts";
+
+  // Fetch the news from the API
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const newsContainer = document.getElementById("news-container");
+
       // Iterate over the news articles and create the HTML structure in English
-      data.forEach(article => {
+      data.forEach((article) => {
         const newsHTML = `
           <div class="col-md-4">
             <div class="news">
               <div class="news-img" style='min-height:250px'>
-                <a href="single-news.html?id=${article.id}" data-triangle=".news-img-overlay">
+                <a href="single-news.html?id=${
+                  article.id
+                }" data-triangle=".news-img-overlay">
                   <span class="news-img-overlay"></span>
-                  <img
-                    src="${article.featured_image}"
-                    alt="${article.title.en}"
-                    width="370"
-                    height="284"
-                  />
+                  <div style='width:370px;height:284px; background-size: cover; background-position: center; background-image:url("${
+                    article.featured_image
+                  }")'></div>
                 </a>
               </div>
               <p class="news-meta">
@@ -34,24 +33,32 @@ document.addEventListener('DOMContentLoaded', function () {
                 <span class="news-author">${article.author.en}</span>
                 <span>-</span>
                 <a class="news-time" href="single-news.html?id=${article.id}">
-                  <time datetime="${new Date(article.published_at).toISOString()}">
-                    ${new Date(article.published_at).toLocaleDateString('en-GB')}
+                  <time datetime="${new Date(
+                    article.published_at,
+                  ).toISOString()}">
+                    ${new Date(article.published_at).toLocaleDateString(
+                      "en-GB",
+                    )}
                   </time>
                 </a>
               </p>
-              <h4 class="news-title">
-                <a href="single-news.html?id=${article.id}">${article.title.en}</a>
-              </h4>
-              <a class="news-link" href="single-news.html?id=${article.id}">Read More</a>
+              <h5 style='font-weight: bold' class="news-title">
+                <a href="single-news.html?id=${article.id}">${
+          article.title.en
+        }</a>
+              </h5>
+              <a class="news-link" href="single-news.html?id=${
+                article.id
+              }">Read More</a>
             </div>
           </div>
         `;
-        
+
         // Append each article to the container
         newsContainer.innerHTML += newsHTML;
       });
     })
-    .catch(error => {
-      console.error('Error fetching news:', error);
+    .catch((error) => {
+      console.error("Error fetching news:", error);
     });
-  });
+});
